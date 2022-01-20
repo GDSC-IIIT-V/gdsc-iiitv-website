fd16c4f787b185994bdbd01f94a2dd52f4642ada
rashiyadhruv <202051153@iiitvadodara.ac.in>
2022-01-20 22:41:09 +0530

    Add Our Team section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Team from "./Components/Team/Team";
+
 
 function App() {
   return (
   <>
-  
   <Navbar />
   <Hero />
-
+  <Team />
   </>
   );
 }
---
+++ src/Components/Team/util/LeadsData.js
@@ -0,0 +1,68 @@
+import gdsc_lead from "../../../assets/images/leads/anubhav_madhav.svg"
+import pr_lead from "../../../assets/images/leads/raghvi_saxena.svg"
+import flutter_lead from "../../../assets/images/leads/kapil_kumar.svg"
+import webdev_lead from "../../../assets/images/leads/shivansh_kumar.svg"
+import android_lead from "../../../assets/images/leads/aman_kothari.svg"
+import arvr_lead from "../../../assets/images/leads/samarth_singh.svg"
+import design_lead from "../../../assets/images/leads/divyam_solanki.svg"
+import mlai_lead from "../../../assets/images/leads/krishna_gaur.svg"
+
+
+export const Leads = [
+  {
+    name: "Anubhav Madhav",
+    position: "GDSC Lead",
+    imgurl: gdsc_lead,
+    githublink: "https://github.com/AnubhavMadhav",
+    linkedinlink: "https://www.linkedin.com/in/anubhav-madhav/",
+  },
+  {
+    name: "Raghvi Saxena",
+    position: "Public Relations Lead",
+    imgurl: pr_lead,
+    githublink: "https://github.com/raghvisaxena",
+    linkedinlink: "https://www.linkedin.com/in/raghvisaxena/",
+  },
+  {
+    name: "Kapil Kumar",
+    position: "Flutter Lead",
+    imgurl: flutter_lead,
+    githublink: "https://github.com/kapilkumar2001",
+    linkedinlink: "https://www.linkedin.com/in/kapilkumar2001/",
+  },
+  {
+    name: "Shivansh Kumar",
+    position: "Web Dev Lead",
+    imgurl: webdev_lead,
+    githublink: "https://github.com/ShivanshKumar",
+    linkedinlink: "https://www.linkedin.com/in/shivansh-kumar-248a751a0/",
+  },
+  {
+    name: "Aman Kothari",
+    position: "Android Lead",
+    imgurl: android_lead,
+    githublink: "https://github.com/AmanKothari20",
+    linkedinlink: "https://www.linkedin.com/in/aman-kothari-9b375319b/",
+  },
+  {
+    name: "Samarth Singh",
+    position: "AR/VR Lead",
+    imgurl: arvr_lead,
+    githublink: "https://github.com/sam20code",
+    linkedinlink: "https://www.linkedin.com/in/samarth-singh-56b5251b4/",
+  },
+  {
+    name: "Divyam Solanki",
+    position: "Design Lead",
+    imgurl: design_lead,
+    githublink: "https://github.com/solankidivyam",
+    linkedinlink: "https://www.linkedin.com/in/divyam-solanki-7a9a12190/",
+  },
+  {
+    name: "Krishna Gaur",
+    position: "ML/AI Lead",
+    imgurl: mlai_lead,
+    githublink: "https://github.com/gaurKrishna",
+    linkedinlink: "https://www.linkedin.com/in/krishna-gaur/",
+  },
+];
