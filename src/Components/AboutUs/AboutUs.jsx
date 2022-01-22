51d6851e265e22a37a02a4bf5346b146d0665f0f
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-01-22 19:21:01 +0530

    add about us responsive design for laptop and desktops p1

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import './AboutUs.css';
 
 // importing all svgs
-import sphere from "../../assets/images/sphere.svg";
-import sphere2 from "../../assets/images/sphere2.svg";
-import sphere3 from "../../assets/images/sphere3.svg";
-import sphere4 from "../../assets/images/sphere4.svg";
+import sphere from "../../assets/images/pipes and spheres/sphere.svg";
+import sphere2 from "../../assets/images/pipes and spheres/sphere2.svg";
+import sphere3 from "../../assets/images/pipes and spheres/sphere3.svg";
+import sphere4 from "../../assets/images/pipes and spheres/sphere4.svg";
 
-import blueLeftCurve from '../../assets/images/BlueLeftCurve.svg';
-import blueRightCurve from '../../assets/images/blueRightCurve.svg';
-import greenRightCurve from '../../assets/images/Group 9.svg';
-import greenLeftCurve from '../../assets/images/Group 10.svg';
-import redCurve from '../../assets/images/RedCurve.svg'
-import yellowPipe from '../../assets/images/YellowPipe.svg';
-import yellowExtension from '../../assets/images/yellowExtension.svg';
+// Pipe imports
+import BlueWholePipe from '../../assets/images/pipes and spheres/BlueWholePipe.svg';
+import RedWholePipe from '../../assets/images/pipes and spheres/RedwholePipe.svg';
+import GreenWholePipe from '../../assets/images/pipes and spheres/GreenWholePipe.svg';
+import yellowPipe from '../../assets/images/pipes and spheres/YellowPipe.svg';
+
+// Red and Yellow Extensions for greater screen sized
+import redExt from '../../assets/images/pipes and spheres/redExtension.svg';
+import yellExt from '../../assets/images/pipes and spheres/yellowExtension.svg';
 
 import Line33 from "../../assets/images/Line 33.svg";
 import Line34 from "../../assets/images/Line 34.svg";
 import Line35 from "../../assets/images/Line 35.svg";
-import Rectangle14 from "../../assets/images/Rectangle 14.svg";
-import Rectangle17 from "../../assets/images/Rectangle 17.svg";
-import Rectangle22 from "../../assets/images/Rectangle 22.svg";
+
+
 
 
 
@@ -31,12 +32,11 @@ export default function AboutUs() {
             {/* Top Div */}
             <div class = "topCont">
                     <img src= {sphere} alt="sphere" id = "sphere"/>
-                    <img src= {redCurve} alt="rectangle-13" id = "rect13" />
-                    <img src= {Rectangle14} alt="rectangle-14" id = "rect14" />
-                    <img src= {greenRightCurve} alt="rectangle-20" id = "rect20"/>
+                    <img src={redExt} alt="red-extension-1880" id = "redExt"/>
+                    <img src={RedWholePipe} alt="redWholePipe" id="redWholePipe" />
                     <img src= {sphere3} alt="sphere" id = "sphere3"/>
-                    <img src= {greenLeftCurve} alt="rectangle-21" id = "rect21"/>
-                    <img src= {Rectangle22} alt="rectangle-22" id = "rect22"/>
+                    <img src={GreenWholePipe} alt="green-whole-pipe" id="greenWholePipe" />
+                    
             </div>
 
             {/* Mid Div containing three boxes and all animations */}
@@ -67,12 +67,11 @@ export default function AboutUs() {
             <div class = "bottomCont">
                     <img src= {sphere2} alt="sphere" id = "sphere2"/>
                     <img src= {sphere4} alt="sphere" id = "sphere4"/>
-                    <img src={yellowExtension} alt="yellow-extension" id="yellowExtension" />
+                    
                     <img src={yellowPipe} alt="yellowpipe" id = "yellowPipe" />
+                    <img src={yellExt} alt="yellow-extension-1880" id = "yellExt"/>
 
-                    <img src= {Rectangle17} alt="rectangle-17" id = "rect17"/>
-                    <img src= {blueLeftCurve} alt="rectangle-18" id = "rect18"/>
-                    <img src={blueRightCurve} alt="rectangle-19" id = "rect19"/>
+                    <img src={BlueWholePipe} alt="blueWholePipe" id="blueWholePipe"/>
 
                     <img src= {Line33} alt="line-33" id = "line33"/>
                     <img src= {Line34} alt="line-34" id = "line34"/>
