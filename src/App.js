99c9dc409bcaad05c6fb445ec43584e28acecf1e
Madhur Gupta <madhurgupta107@gmail.com>
2022-01-26 18:51:07 +0530

    adds responsiveness to thread connections in Events section

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import './App.css'
-import Hero from './Components/Hero/Hero'
-import Navbar from './Components/Navbar/Navbar'
-import AboutUs from './Components/AboutUs/AboutUs'
-import Team from './Components/Team/Team'
-import Events from './Components/Events/Events'
-import Video from './Components/Video/Video'
-import ContactUs from './Components/ContactUs/ContactUs'
-import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
+import "./App.css";
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import Team from "./Components/Team/Team";
+import Events from "./Components/Events/Events";
+import Video from "./Components/Video/Video";
+import ContactUs from "./Components/ContactUs/ContactUs";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import { render } from "react-dom";
 import { Link } from "react-router-dom";
-import Alumini from './Our team/Alumini'
-import Coreteam from './Our team/Coreteam'
-import Mentors from './Our team/Mentors'
-import Volunteers from './Our team/Volunteers'
+import Alumini from "./Our team/Alumini";
+import Coreteam from "./Our team/Coreteam";
+import Mentors from "./Our team/Mentors";
+import Volunteers from "./Our team/Volunteers";
 
 function App() {
   return (
-    <Router><Routes><Route path="/" element={<>
-      <Navbar />
-      <Hero />
-      <AboutUs />
-      <Team />
-      <Events />
-      <Video />
-      <ContactUs />
-    </>}></Route>
-    
-    <Route path="/Alumini" element={<Alumini/>}></Route>
-    <Route path="/Coreteam" element={<Coreteam/>}></Route>
-    <Route path="/Mentors" element={<Mentors/>}></Route>
-    <Route path="/Volunteers" element={<Volunteers/>}></Route>
-       
-    </Routes></Router>
-   
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {/* <Navbar />
+              <Hero />
+              <AboutUs />
+              <Team /> */}
+              <Events />
+              {/* <Video />
+              <ContactUs /> */}
+            </>
+          }
+        ></Route>
+
+        <Route path="/Alumini" element={<Alumini />}></Route>
+        <Route path="/Coreteam" element={<Coreteam />}></Route>
+        <Route path="/Mentors" element={<Mentors />}></Route>
+        <Route path="/Volunteers" element={<Volunteers />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
