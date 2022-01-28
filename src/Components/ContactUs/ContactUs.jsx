1e876537f2f4db2e39759d750f635eefc05e228d
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-01-28 12:00:11 +0530

    add major changes in contact us

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -6,8 +6,7 @@ import linkedin from '../../assets/images/LinkedIn.png'
 import discord from '../../assets/images/discord.png'
 import twitter from '../../assets/images/twitter.png'
 import contactusdot from '../../assets/images/contactusdot.png'
-import Dot from './Dot'
-import Footer from '../Footer/Footer'
+
 
 function ContactUs() {
   return (
@@ -18,6 +17,7 @@ function ContactUs() {
       </div>
       <div className='contactus-button'>
         <button className='button-contact'> Contact Us</button>
+        
         <div className='socialmedia'>
           <a href='https://www.youtube.com/' target='_blank' rel='noreferrer'>
             <img src={linkedin} alt='LinkedIn' className='iconimg' />
@@ -31,11 +31,16 @@ function ContactUs() {
             <img src={discord} alt='discord' className='iconimg' />
           </a>
         </div>
+        <div className="footer">
+      <span className='names'>
+          Made with ❤️ by <a href="https://www.linkedin.com/in/madhur-gupta-07408320b/" target="_blank" rel="noreferrer" className='footer-links'>Madhur Gupta</a>, <a href="https://www.linkedin.com/in/dhruv1201/" target="_blank" rel="noreferrer" className='footer-links'>Dhruv Dave</a>,<a href="https://www.linkedin.com/in/dhruv-rashiya-6164791b8/" target="_blank" rel="noreferrer" className='footer-links'>Dhruv Rashiya</a>,<a href="https://www.linkedin.com/in/riya-parikh-27b345196/" target="_blank" rel="noreferrer" className='footer-links'>Riya Parikh</a>,<a href="https://www.linkedin.com/in/parth-chandravadiya-0379a0227/" target="_blank" rel="noreferrer" className='footer-links'>Parth Chandravadiya</a>,<a href="https://www.linkedin.com/in/kewal-delhiwala-a4ba24201" target="_blank" rel="noreferrer" className='footer-links'>Kewal Delhiwala</a>
+        </span>
+      </div>
+    </div>
         <img src={contactusdot} alt="dot" className='dot'/>
       </div>
 
-      <Footer/>
-    </div>
+      
   )
 }
 
