89106f55bd8af033f2e56dec46fcbe44dc9e99ce
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-06-06 15:01:02 +0530

    feat: final addition of event thumbnails and fixed height glitch

--- src/Components/Events/util/EventsData.js
+++ src/Components/Events/util/EventsData.js
@@ -7,38 +7,38 @@ import event6 from "../../../assets/images/events/event6.jpg";
 
 const EventsData = [
   {
-    thumbnail: event3,
+    thumbnail: 'https://i.imgur.com/g7MUpDC.jpeg',
     title: "The Beginning",
     date: "20/09/2021 6pm",
     text: "First ever introductory meeting full of fun and knowledge for the year 2021-22.",
   },
   {
-    thumbnail: event1,
+    thumbnail: 'https://i.imgur.com/DHGezFS.jpeg',
     title: "Life In CSE",
     date: "21/01/2022 6pm",
     text: "Get ready to know of this terra-incognita of infinite from a denizen of the world of computers! 😃👨‍💻",
   },
   {
-    thumbnail: event2,
+    thumbnail: 'https://i.imgur.com/LWq2NmI.jpeg',
     title: "Google STEP Intern",
     date: "04/10/2021 6pm",
     text: "Get to know all about Google Step Internship Program from the already experienced Ms. Dhriti Chopra",
   },
   
   {
-    thumbnail: event4,
+    thumbnail: 'https://i.imgur.com/PXoOKUA.jpeg',
     title: "Introduction to AR/VR",
     date: "02/10/2021 6pm",
     text: "Introduction to AR/VR was a workshop where basic knowledge regarding Augmented Reality and Unity Engine was given.",
   },
   {
-    thumbnail: event5,
+    thumbnail: 'https://i.imgur.com/FA4YA6H.jpeg',
     title: "Android Study Jam",
     date: "16/12/2021 6pm",
     text: "This session enabled students to start their journey with Android Development, so as to spreading awareness about this amazing domain",
   },
   {
-    thumbnail: event5,
+    thumbnail: 'https://i.imgur.com/mHrWSh8.jpeg',
     title: "Flutter Festival",
     date: "14/02/2022 6pm",
     text: "This session enables students to know about flutter and was a complete 16 days of flutter sessions with tons of knowledge",
